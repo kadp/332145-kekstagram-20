1670c30cc0b67e86cc695b6548097108aead9968
KadP <kadp@yandex.ru>
2020-06-10 23:46:13 +0300

    Исправил функцию формирования массива комментариев, а так же вывод колличесва комментариев.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 'use strict';
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
-var messages = ['Всё отлично!',
+var AVATAR_NAMES = ['Леонардо', 'Рафаэль', 'Донателло', 'Микеланджело', 'Сплинтер', 'Эйприл О’Нил'];
+var MESSAGES = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-var photoDescription = '';
-var avatarNames = ['Леонардо', 'Рафаэль', 'Донателло', 'Микеланджело', 'Сплинтер', 'Эйприл О’Нил'];
+
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-var pictureContainer = document.querySelector('.pictures.container');
+var pictureContainer = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
+var photoDescription = '';
 
 var getRandomNumber = function (min, max) {
   min = Math.ceil(min);
@@ -21,8 +22,8 @@ var getRandomNumber = function (min, max) {
 };
 
 var getRandomName = function () {
-  var i = getRandomNumber(0, avatarNames.length - 1);
-  return avatarNames[i];
+  var i = getRandomNumber(0, AVATAR_NAMES.length - 1);
+  return AVATAR_NAMES[i];
 };
 
 var getLikes = function () {
@@ -30,16 +31,16 @@ var getLikes = function () {
 };
 
 var getRandomComments = function () {
-  var i = getRandomNumber(0, messages.length - 1);
-  return messages[i];
+  var i = getRandomNumber(0, MESSAGES.length - 1);
+  return MESSAGES[i];
 };
 
-var getComments = function () {
+var getMessages = function () {
   var comment = '';
   if (getRandomNumber(1, 2) === 1) {
-    comment += getRandomComments(messages);
+    comment += getRandomComments(MESSAGES);
   } else {
-    comment = getRandomComments(messages) + ' ' + getRandomComments(messages);
+    comment = getRandomComments(MESSAGES) + ' ' + getRandomComments(MESSAGES);
   }
   return comment;
 };
@@ -57,10 +58,13 @@ var getAvatar = function () {
 var makeComments = function (avatar, comment, name) {
   var element = {};
   var comments = [];
-  element.avatar = avatar;
-  element.message = comment;
-  element.name = name;
-  comments.push(element);
+  var commentsCount = getRandomNumber(0, 10);
+  for (var i = 0; i < commentsCount; i++) {
+    element.avatar = avatar;
+    element.message = comment;
+    element.name = name;
+    comments.push(element);
+  }
   return comments;
 };
 
@@ -76,7 +80,7 @@ var generateElement = function (url, description, likes, comments) {
 var generateArray = function () {
   var generatedArray = [];
   for (var i = 1; i <= 25; i++) {
-    var newElement = generateElement(getUrl(i), photoDescription, getLikes(), makeComments(getAvatar(), getComments(), getRandomName()));
+    var newElement = generateElement(getUrl(i), photoDescription, getLikes(), makeComments(getAvatar(), getMessages(), getRandomName()));
     generatedArray.push(newElement);
   }
   return generatedArray;
@@ -88,7 +92,7 @@ var getPictureClone = function (n) {
   var clonedElement = pictureTemplate.cloneNode(true);
   clonedElement.querySelector('img').src = pictureList[n].url;
   clonedElement.querySelector('.picture__likes').textContent = pictureList[n].likes;
-  clonedElement.querySelector('.picture__comments').textContent = pictureList[n].comments[0].message;
+  clonedElement.querySelector('.picture__comments').textContent = pictureList[n].comments.length;
   return clonedElement;
 };
 
