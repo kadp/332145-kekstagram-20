716af5a92deeff5f68361a71a9c6a64cc2e39dca
KadP <kadp@yandex.ru>
2020-06-17 20:40:22 +0300

    Исправил замечания наставника.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
+var COMMENTS_RANGE = 33;
+var PICTURES_LIST_RANGE = 25;
+var AVATAR_RANGE = 6;
 var AVATAR_NAMES = ['Леонардо', 'Рафаэль', 'Донателло', 'Микеланджело', 'Сплинтер', 'Эйприл О’Нил'];
 var MESSAGES = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -10,10 +13,10 @@ var MESSAGES = ['Всё отлично!',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+var picturesList = [];
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 var pictureContainer = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
-var photoDescription = '';
 
 var getRandomNumber = function (min, max) {
   min = Math.ceil(min);
@@ -21,86 +24,55 @@ var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getRandomName = function () {
-  var i = getRandomNumber(0, AVATAR_NAMES.length - 1);
-  return AVATAR_NAMES[i];
-};
-
-var getLikes = function () {
-  return getRandomNumber(MIN_LIKES, MAX_LIKES);
-};
-
-var getRandomComments = function () {
-  var i = getRandomNumber(0, MESSAGES.length - 1);
-  return MESSAGES[i];
-};
-
 var getMessages = function () {
-  var comment = '';
-  if (getRandomNumber(1, 2) === 1) {
-    comment += getRandomComments(MESSAGES);
-  } else {
-    comment = getRandomComments(MESSAGES) + ' ' + getRandomComments(MESSAGES);
+  var message = MESSAGES[getRandomNumber(0, MESSAGES.length - 1)];
+  if (getRandomNumber(0, 1)) {
+    message += ' ' + MESSAGES[getRandomNumber(0, MESSAGES.length - 1)];
   }
-  return comment;
+  return message;
 };
 
-var getUrl = function (n) {
-  var url = 'photos/' + n + '.jpg';
-  return url;
-};
-
-var getAvatar = function () {
-  var userAvatar = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
-  return userAvatar;
-};
-
-var makeComments = function (avatar, comment, name) {
-  var element = {};
+var getComments = function () {
   var comments = [];
-  var commentsCount = getRandomNumber(0, 10);
-  for (var i = 0; i < commentsCount; i++) {
-    element.avatar = avatar;
-    element.message = comment;
-    element.name = name;
-    comments.push(element);
+  for (var i = 0; i < getRandomNumber(0, COMMENTS_RANGE); i++) {
+    var commentsInfo = {};
+    commentsInfo.avatar = 'img/avatar-' + getRandomNumber(1, AVATAR_RANGE) + '.svg';
+    commentsInfo.message = getMessages();
+    commentsInfo.name = AVATAR_NAMES[getRandomNumber(0, AVATAR_NAMES.length - 1)];
+    comments.push(commentsInfo);
   }
   return comments;
 };
 
-var generateElement = function (url, description, likes, comments) {
-  var element = {};
-  element.url = url;
-  element.description = description;
-  element.likes = likes;
-  element.comments = comments;
-  return element;
+var getPictureInfo = function (i) {
+  return {
+    url: 'photos/' + i + '.jpg',
+    description: '',
+    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+    comments: getComments()
+  };
 };
 
-var generateArray = function () {
-  var generatedArray = [];
-  for (var i = 1; i <= 25; i++) {
-    var newElement = generateElement(getUrl(i), photoDescription, getLikes(), makeComments(getAvatar(), getMessages(), getRandomName()));
-    generatedArray.push(newElement);
+var getPicturesList = function () {
+  for (var i = 1; i <= PICTURES_LIST_RANGE; i++) {
+    picturesList.push(getPictureInfo(i));
   }
-  return generatedArray;
 };
-
-var pictureList = generateArray();
 
 var getPictureClone = function (n) {
   var clonedElement = pictureTemplate.cloneNode(true);
-  clonedElement.querySelector('img').src = pictureList[n].url;
-  clonedElement.querySelector('.picture__likes').textContent = pictureList[n].likes;
-  clonedElement.querySelector('.picture__comments').textContent = pictureList[n].comments.length;
+  clonedElement.querySelector('img').src = picturesList[n].url;
+  clonedElement.querySelector('.picture__likes').textContent = picturesList[n].likes;
+  clonedElement.querySelector('.picture__comments').textContent = picturesList[n].comments.length;
   return clonedElement;
 };
 
 var getFullDomOfPicture = function () {
-  for (var i = 0; i < pictureList.length; i++) {
+  for (var i = 0; i < picturesList.length; i++) {
     fragment.appendChild(getPictureClone(i));
   }
   pictureContainer.appendChild(fragment);
 };
 
+getPicturesList();
 getFullDomOfPicture();
