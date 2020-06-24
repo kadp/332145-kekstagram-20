0a52b0db3042ffddaedc643d98d386d4280fca4b
KadP <kadp@yandex.ru>
2020-06-24 19:59:09 +0300

    Черновик

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ var getPictureClone = function (n) {
   return clonedPicture;
 };
 
-var getFullDomOfPicture = function () {
+var renderPicturesList = function () {
   for (var i = 0; i < picturesList.length; i++) {
     fragment.appendChild(getPictureClone(i));
   }
@@ -79,4 +79,172 @@ var getFullDomOfPicture = function () {
 };
 
 getPicturesList();
-getFullDomOfPicture();
+renderPicturesList();
+
+//Временный комментарий - разделение кода 4-го модуля.
+
+var uploadForm = document.querySelector('#upload-file');
+var body = document.querySelector('body');
+var formEditPicture = document.querySelector('.img-upload__overlay');
+var buttonUploadCancel = document.querySelector('#upload-cancel');
+var sliderDeepEffect = document.querySelector('.effect-level__pin');
+var effectLevel = document.querySelector('.effect-level__value');
+var effectLevelLine = document.querySelector('.effect-level__depth');
+var scaleControlSmaller = document.querySelector('.scale__control--smaller');
+var scaleControlBigger = document.querySelector('.scale__control--bigger');
+var scaleControlValue = document.querySelector('.scale__control--value');
+var fieldsetDeepEffect = document.querySelector('.img-upload__effect-level');
+
+var imgUploadPreview = document.querySelector('.img-upload__preview');
+var effectsList = document.querySelector('.effects__list');
+var effectsListType = document.querySelectorAll('input[type="radio"]');
+
+var MIN_HASHTAG_LENGTH = 2;
+var MAX_HASHTAGE_LENGTH = 20;
+var MAX_HASHTAGE_AMOUNT = 5;
+var hashtagRe = /^#[a-zа-яA-ZА-Я0-9]*$/gm;
+
+var hashtagsInput = document.querySelector('.text__hashtags');
+var uploadSubmitButton = document.querySelector('#upload-submit');
+
+hashtagsInput.addEventListener('invalid', function () {
+  if (hashtagsInput.validity.tooShort) {
+    hashtagsInput.setCustomValidity('ХэшТег должен состоять минимум из 2-х символов');
+  } else if (hashtagsInput.validity.tooLong) {
+    hashtagsInput.setCustomValidity('ХэшТег не должен превышать 20-ти символов, включая #');
+  } else {
+    hashtagsInput.setCustomValidity('');
+  }
+});
+
+// код данной функции можно просто добавить как еще одна веттка else if на 'invalid'?
+var validateHashtags = function () {
+  var hashtagsArray = hashtagsInput.value.split(' ', 5);
+  if (hashtagsArray.length > MAX_HASHTAGE_AMOUNT) {
+    hashtagsInput.setCustomValidity('ХешТэгов не может быть больше ' + MAX_HASHTAGE_AMOUNT);
+  } else {
+    for (var i = 0; i < hashtagsArray.length; i++) {
+      hashtagRe.exec(hashtagsArray[i]);
+    }
+  }
+};
+
+uploadSubmitButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (validateHashtags()) {
+    console.log('ok');
+  } else {
+    console.log('bad');
+  }
+
+});
+
+hashtagsInput.addEventListener('input', function () {
+  var valueLength = hashtagsInput.value.length;
+
+  if (valueLength < MIN_HASHTAG_LENGTH) {
+    hashtagsInput.setCustomValidity('Ещё ' + (MIN_HASHTAG_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_HASHTAGE_LENGTH) {
+    hashtagsInput.setCustomValidity('Удалите лишние ' + (valueLength - MIN_HASHTAG_LENGTH) + ' симв.');
+  } else {
+    hashtagsInput.setCustomValidity('');
+  }
+
+});
+
+effectsList.addEventListener('click', function () {
+  for (var i = 0; i < effectsListType.length; i++) {
+    if (effectsListType[i].checked) {
+      fieldsetDeepEffect.classList.remove('hidden');
+      switch (effectsListType[i].value) {
+        case 'none':
+          fieldsetDeepEffect.classList.add('hidden');
+          imgUploadPreview.style.filter = 'none';
+          break;
+        case 'chrome':
+          imgUploadPreview.style.filter = 'grayscale(' + effectLevel.value + '%)';
+          setSliderDefaultPosition();
+          break;
+        case 'sepia':
+          imgUploadPreview.style.filter = 'sepia(100%)';
+          setSliderDefaultPosition();
+          break;
+        case 'marvin':
+          imgUploadPreview.style.filter = 'invert(' + effectLevel.value + '%)';
+          setSliderDefaultPosition();
+          break;
+        case 'phobos':
+          imgUploadPreview.style.filter = 'blur(10px)';
+          setSliderDefaultPosition();
+          break;
+        case 'heat':
+          imgUploadPreview.style.filter = 'brightness(' + effectLevel.value / 100 + ')';
+          setSliderDefaultPosition();
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+});
+
+uploadForm.addEventListener('change', function () {
+  if (uploadForm.value) {
+    openUploadForm();
+  }
+});
+
+scaleControlSmaller.addEventListener('click', function () {
+  if (parseInt(scaleControlValue.value, 10) > 25) {
+    scaleControlValue.value = parseInt(scaleControlValue.value, 10) - 25 + '%';
+    imgUploadPreview.style.transform = 'scale(' + parseInt(scaleControlValue.value, 10) / 100 + ')';
+  }
+});
+
+scaleControlBigger.addEventListener('click', function () {
+  if (parseInt(scaleControlValue.value, 10) < 100) {
+    scaleControlValue.value = parseInt(scaleControlValue.value, 10) + 25 + '%';
+    imgUploadPreview.style.transform = 'scale(' + parseInt(scaleControlValue.value, 10) / 100 + ')';
+  }
+});
+
+var setSliderDefaultPosition = function () {
+  effectLevel.value = parseInt(effectLevel.value, 10);
+  sliderDeepEffect.style.left = effectLevel.value + '%';
+  effectLevelLine.style.width = effectLevel.value + '%';
+};
+
+sliderDeepEffect.addEventListener('mouseup', function () {
+  effectLevel.value = parseInt(effectLevel.value, 10);
+  if (effectLevel.value > 0) {
+    effectLevel.value -= 20;
+    sliderDeepEffect.style.left = effectLevel.value + '%';
+    effectLevelLine.style.width = effectLevel.value + '%';
+  }
+});
+
+buttonUploadCancel.addEventListener('click', function () {
+  closeUploadForm();
+});
+
+var onUploadFormEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+};
+
+var closeUploadForm = function () {
+  formEditPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uploadForm.value = '';
+  document.removeEventListener('keydown', onUploadFormEscPress);
+};
+
+var openUploadForm = function () {
+  formEditPicture.classList.remove('hidden');
+  fieldsetDeepEffect.classList.add('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onUploadFormEscPress);
+};
